aa7557ea2cda896090821c1c8b0f9e12acf4a4ab
Leonardo Trujillo <leonardotrujillor@gmail.com>
2016-04-21 12:48:16 -0500

    probando controlador

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('appFlisol', []);
+
+
+app.controller('comicController',[function(){
+    this.comic = {
+        "id": "001",
+        "name": "Deadpool",
+        "price": "14.00",
+        "description": "Esta es la descripción de el comic de Deadpool",
+        "thumbnail": "deadpool.jpg"
+    };
+    console.log(this.comic);
+}])
