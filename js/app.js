3fab8503eb95df8df440adc6fe2893c7ddec6e96
Leonardo Trujillo <leonardotrujillor@gmail.com>
2016-04-21 15:22:30 -0500

    vista de comic info

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
-var app = angular.module('appFlisol', []);
+var app = angular.module('appFlisol', ['ngRoute']);
 
 
-app.controller('comicController',[function(){
-    this.comic = {
+app.config(['$routeProvider', function($routeProvider){ //esta es la configuración de el roter
+  $routeProvider
+    .when('/',{
+      templateUrl: 'views/comics.html',
+      controller: 'comicController'
+    })
+    .when('/comic/:name', {
+      templateUrl: 'views/comic-info.html',
+      controller: 'infoController'
+    })
+    .otherwise({
+        redirectTo: '/'
+      });
+}])
+//Controladores de la aplicacion
+.controller('comicController',['$scope', function($scope){
+    $scope.comic = {
         "id": "001",
         "name": "Deadpool",
         "price": "14.00",
         "description": "Esta es la descripción de el comic de Deadpool",
         "thumbnail": "deadpool.jpg"
     };
+}])
+
+.controller('infoController',['$scope', '$routeParams', function($scope, $routeParams){
+  $scope.name = $routeParams.name;
 }])
